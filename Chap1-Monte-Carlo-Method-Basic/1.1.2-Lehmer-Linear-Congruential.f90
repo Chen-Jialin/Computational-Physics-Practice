fa715026997b4733f27dc0fe28f06e9033e345b6
Chen Jialin <chenjl@shanghaitech.edu.cn>
2021-07-11 23:11:16 +0800

    1.1.2 Lehmer 线性同余法

---
+++ Chap1-Monte-Carlo-Method-Basic/1.1.2-Lehmer-Linear-Congruential.f90
@@ -0,0 +1,52 @@
+program main
+    implicit none
+    integer, parameter :: li = selected_int_kind(10)
+    integer, parameter :: dp = selected_real_kind(8)
+
+    integer(li) :: a, b, m! 乘子, 增量, 模数
+    common /group1/ a, b, m
+    real(dp) :: x, y, z
+    integer :: i
+    real(dp) tmp
+
+    interface
+        function Lehmer_linear_congruential(seed)
+            implicit none
+            integer, parameter :: dp = selected_real_kind(8)
+
+            integer, intent(in), optional :: seed
+            real(dp) :: Lehmer_linear_congruential
+        end function
+    end interface
+
+    m = 2**31
+    a = 2**16 + 3
+    b = 0
+    open(unit=11, file='1.1.2-IBM-RANDU.txt', status='replace')
+    do i = 1,10000
+        x = Lehmer_linear_congruential()
+        y = Lehmer_linear_congruential()
+        z = Lehmer_linear_congruential()
+        write(11, '(3f16.8)') x, y, z
+    end do
+end program main
+
+function Lehmer_linear_congruential(seed)
+    ! Lehmer 线性同余法产生 [0,1) 区间上的均匀随机数
+    implicit none
+    integer, parameter :: li = selected_int_kind(10)
+    integer, parameter :: dp = selected_real_kind(8)
+
+    integer(li) :: a, b, m! 乘子, 增量, 模数
+    common /group1/ a, b, m
+    integer, intent(in), optional :: seed
+    real(dp) :: Lehmer_linear_congruential
+    integer(li), save :: I_n = -1
+    if (present(seed)) then
+        I_n = seed
+    else if (I_n == -1) then
+        I_n = 1
+    end if
+    I_n = mod((a * I_n + b), m)
+    Lehmer_Linear_Congruential = I_n / dble(m)
+end function Lehmer_Linear_Congruential
