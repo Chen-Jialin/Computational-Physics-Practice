46bd6ebbf04ff8204d36c66496e97f43c4227f2a
Chen Jialin <chenjl@shanghaitech.edu.cn>
2021-07-22 13:36:30 +0800

    添加 Poisson 分布

--- Chap1-Monte-Carlo-Method-Basic/1.2.1.1-Discrete-Variable-Distribution.f90
+++ Chap1-Monte-Carlo-Method-Basic/1.2.1.1-Discrete-Variable-Distribution.f90
@@ -11,9 +11,23 @@ program main
 
             real(dp) :: Discrete_varible
         end function Discrete_varible
+
+        function Poisson(lambda)
+            ! Poisson 分布
+            implicit none
+            integer, parameter :: dp = selected_real_kind(8)
+        
+            integer, intent(in) :: lambda
+            real(dp) :: Poisson
+        end function Poisson
     end interface
+
     do i = 1, 100
         write(*,*) Discrete_varible()
+    end do
+
+    do i = 1, 100
+        write(*,*) Poisson(1)
     end do
 end program main
 
@@ -31,8 +45,46 @@ function Discrete_varible()
 
     call random_number(xi)
     i = 1
-    do while (.not. ((sum(p(:i - 1)) <= xi) .and. (xi < sum(p(:i)))))
+    do while (xi >= sum(p(:i)))
         i = i + 1
     end do
     Discrete_varible = x(i)
 end function Discrete_varible
+
+function Poisson(lambda)
+    ! Poisson 分布
+    implicit none
+    integer, parameter :: dp = selected_real_kind(8)
+
+    integer, intent(in) :: lambda
+    real(dp) :: Poisson
+
+    real(dp) :: xi
+    integer :: i
+    real(dp) :: sum_of_p_n
+
+    call random_number(xi)
+    xi = xi * exp(dble(lambda))
+    i = 0
+    sum_of_p_n = lambda**i / dble(factorial(i))
+    do while (xi >= sum_of_p_n)
+        i = i + 1
+        sum_of_p_n = sum_of_p_n + lambda**i / dble(factorial(i))
+    end do
+    Poisson = i
+
+    contains
+        function factorial(n)
+            implicit none
+
+            integer, intent(in) :: n
+            integer :: factorial
+
+            integer :: i
+
+            factorial = 1
+            do i = 1, n
+                factorial = factorial * i
+            end do
+        end function factorial
+end function Poisson
